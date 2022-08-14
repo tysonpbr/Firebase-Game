78bdeab782176221aa7feb20b8873a14fb14234c
Tyson Brown <90238566+tysonpbr@users.noreply.github.com>
2022-08-13 22:17:34 -0700

    Add files via upload

---
+++ Firebase Game 2022-08-13/app.js
@@ -0,0 +1,724 @@
+const mapData = {
+  minX: 1,
+  maxX: 1000,
+  minY: 1,
+  maxY: 1000,
+  blockedSpaces: {
+    /* PERIMETER */
+    "30x12": true,
+    "28x9": true,
+    "29x9": true,
+    "28x10": true,
+    "29x10": true,
+    "28x11": true,
+    "21x12": true,
+    "22x13": true,
+    "23x11": true,
+    "23x12": true,
+    "24x13": true,
+    "25x11": true,
+    "25x12": true,
+    "26x13": true,
+    "27x13": true,
+    "28x13": true,
+    "29x13": true, 
+    "25x7": true,
+    "25x8": true,
+    "26x8": true,
+    "27x8": true,
+    "23x6": true,
+    "24x6": true,
+    "22x7": true,
+    "23x8": true,
+    "17x6": true,
+    "18x6": true,
+    "19x6": true,
+    "20x6": true,
+    "21x7": true,
+    "21x8": true,
+    "21x9": true,
+    "22x9": true,
+    "23x9": true,
+    "9x7": true,
+    "10x7": true,
+    "11x7": true,
+    "12x7": true,
+    "13x7": true,
+    "14x7": true,
+    "15x7": true,
+    "16x7": true,
+    "16x8": true,
+    "16x9": true,
+    "9x13": true,
+    "10x13": true,
+    "11x13": true,
+    "12x13": true,
+    "13x13": true,
+    "14x13": true,
+    "15x13": true,
+    "15x13": true,
+    "9x14": true,
+    "10x14": true,
+    "11x15": true,
+    "12x15": true,
+    "13x15": true,
+    "14x15": true,
+    "15x15": true,
+    "16x11": true,
+    "16x12": true,
+    "16x13": true,
+    "16x14": true,
+    "16x15": true,
+    "16x16": true,
+    "8x8": true,
+    "8x9": true,
+    "8x10": true,
+    "8x11": true,
+    "8x12": true,
+    "8x13": true,
+    "8x14": true,
+    "8x15": true,
+    "8x16": true,
+    "8x17": true,
+    "8x18": true,
+    "9x19": true,
+    "10x17": true,
+    "10x18": true,
+    "11x17": true,
+    "11x18": true,
+    "12x19": true,
+    "14x17": true,
+    "14x18": true,
+    "13x17": true,
+    "13x18": true,
+    "15x19": true,
+    "16x20": true,
+    "16x19": true,
+    "16x18": true,
+    "9x21": true,
+    "10x21": true,
+    "11x21": true,
+    "12x21": true,
+    "13x21": true,
+    "14x21": true,
+    "15x21": true,
+    "16x21": true,
+    "9x25": true,
+    "9x26": true,
+    "9x27": true,
+    "9x28": true,
+    "9x29": true,
+    "9x30": true,
+    "9x31": true,
+    "9x32": true,
+    "9x33": true,
+    "9x43": true,
+    "3x35": true,
+    "7x34": true,
+    "6x34": true,
+    "5x34": true,
+    "4x34": true,
+    "3x33": true,
+    "7x32": true,
+    "6x32": true,
+    "5x32": true,
+    "4x32": true,
+    "3x31": true,
+    "7x30": true,
+    "6x30": true,
+    "5x30": true,
+    "4x30": true,
+    "3x29": true,
+    "7x28": true,
+    "6x28": true,
+    "5x28": true,
+    "4x28": true,
+    "3x27": true,
+    "7x26": true,
+    "6x26": true,
+    "5x26": true,
+    "4x26": true,
+    "3x25": true,
+    "7x24": true,
+    "6x24": true,
+    "5x24": true,
+    "4x24": true,
+    "3x23": true,
+    "7x22": true,
+    "6x22": true,
+    "5x22": true,
+    "4x22": true,
+    "8x22": true,
+    "71x37": true,
+    "70x37": true,
+    "69x38": true,
+    "68x38": true,
+    "67x38": true,
+    "66x38": true,
+    "65x38": true,
+    "64x38": true,
+    "63x38": true,
+    "62x38": true,
+    "61x38": true,
+    "60x38": true,
+    "59x38": true,
+    "58x38": true,
+    "57x38": true,
+    "56x38": true,
+    "55x38": true,
+    "54x38": true,
+    "53x38": true,
+    "52x38": true,
+    "51x38": true,
+    "50x38": true,
+    "49x38": true,
+    "48x38": true,
+    "47x38": true,
+    "46x38": true,
+    "45x38": true,
+    "44x38": true,
+    "43x38": true,
+    "42x38": true,
+    "41x38": true,
+    "40x38": true,
+    "39x38": true,
+    "38x38": true,
+    "37x38": true,
+    "36x38": true,
+    "35x38": true,
+    "34x38": true,
+    "33x38": true,
+    "32x38": true,
+    "31x38": true,
+    "30x38": true,
+    "29x38": true,
+    "28x38": true,
+    "27x38": true,
+    "26x38": true,
+    "25x38": true,
+    "24x38": true,
+    "23x38": true,
+    "22x38": true,
+    "21x38": true,
+    "20x38": true,
+    "19x38": true,
+    "18x38": true,
+    "17x38": true,
+    "16x38": true,
+    "15x38": true,
+    "14x38": true,
+    "13x38": true,
+    "12x38": true,
+    "11x38": true,
+    "10x38": true,
+    "9x38": true,
+    "8x37": true,
+    "7x36": true,
+    "6x36": true,
+    "5x36": true,
+    "4x36": true,
+    "87x48": true,
+    "87x47": true,
+    "87x46": true,
+    "87x45": true,
+    "77x41": true,
+    "78x41": true,
+    "79x41": true,
+    "80x41": true,
+    "81x41": true,
+    "82x41": true,
+    "84x41": true,
+    "83x41": true,
+    "83x41": true,
+    "84x41": true,
+    "85x41": true,
+    "86x41": true,
+    "87x41": true,
+    "77x42": true,
+    "78x42": true,
+    "79x42": true,
+    "80x42": true,
+    "81x42": true,
+    "82x42": true,
+    "84x42": true,
+    "83x42": true,
+    "83x42": true,
+    "84x42": true,
+    "85x42": true,
+    "86x42": true,
+    "87x42": true,
+    "72x41": true,
+    "73x41": true,
+    "74x41": true,
+    "75x41": true,
+    "72x42": true,
+    "73x42": true,
+    "74x42": true,
+    "75x42": true,
+    "71x38": true,
+    "71x39": true,
+    "71x40": true,
+    "71x41": true,
+    "71x42": true,
+    "71x43": true,
+    "71x44": true,
+    "71x45": true,
+    "71x46": true,
+    "71x47": true,
+    "71x48": true,
+    "72x49": true,
+    "73x49": true,
+    "74x49": true,
+    "75x49": true,
+    "76x49": true,
+    "77x49": true,
+    "78x49": true,
+    "79x49": true,
+    "80x49": true,
+    "81x49": true,
+    "82x49": true,
+    "84x48": true,
+    "83x48": true,
+    "83x49": true,
+    "82x50": true,
+    "81x51": true,
+    "80x52": true,
+    "79x53": true,
+    "79x54": true,
+    "79x55": true,
+    "79x56": true,
+    "80x57": true,
+    "81x58": true,
+    "82x59": true,
+    "83x60": true,
+    "84x61": true,
+    "85x61": true,
+    "86x61": true,
+    "87x61": true,
+    "88x60": true,
+    "89x59": true,
+    "90x58": true,
+    "91x57": true,
+    "92x56": true,
+    "92x55": true,
+    "92x54": true,
+    "92x53": true,
+    "93x53": true,
+    "94x53": true,
+    "95x53": true,
+    "96x52": true,
+    "96x51": true,
+    "96x50": true,
+    "96x49": true,
+    "96x48": true,
+    "96x47": true,
+    "96x46": true,
+    "96x45": true,
+    "96x44": true,
+    "96x43": true,
+    "96x42": true,
+    "96x41": true,
+    "95x40": true,
+    "95x39": true,
+    "94x39": true,
+    "94x38": true,
+    "93x38": true,
+    "93x37": true,
+    "92x37": true,
+    "92x36": true,
+    "92x35": true,
+    "92x34": true,
+    "95x38": true,
+    "96x38": true,
+    "97x38": true,
+    "98x38": true,
+    "99x38": true,
+    "100x38": true,
+    "101x38": true,
+    "102x38": true,
+    "103x37": true,
+    "103x36": true,
+    "104x35": true,
+    "104x34": true,
+    "104x33": true,
+    "104x32": true,
+    "104x31": true,
+    "103x30": true,
+    "102x30": true,
+    "101x30": true,
+    "100x30": true,
+    "99x30": true,
+    "98x30": true,
+    "97x30": true,
+    "96x30": true,
+    "95x30": true,
+    "94x30": true,
+    "93x30": true,
+    "92x30": true,
+    "91x30": true,
+    "91x31": true,
+    "90x29": true,
+    "89x28": true,
+    "88x27": true,
+    "87x26": true,
+    "86x26": true,
+    "85x26": true,
+    "84x26": true,
+    "83x26": true,
+    "82x26": true,
+    "81x26": true,
+    "81x25": true,
+    "81x24": true,
+    "81x23": true,
+    "81x22": true,
+    "82x25": true,
+    "83x25": true,
+    "84x25": true,
+    "85x25": true,
+    "86x25": true,
+    "87x25": true,
+
+    /* BOTTOM HOLLOW GAP */
+    "93x50": true,
+    "93x49": true,
+    "93x48": true,
+    "93x47": true,
+    "93x46": true,
+    "93x45": true,
+    "93x44": true,
+    "93x43": true,
+    "93x42": true,
+    "93x41": true,
+    "92x50": true,
+    "92x49": true,
+    "92x48": true,
+    "92x47": true,
+    "92x46": true,
+    "92x45": true,
+    "92x44": true,
+    "92x43": true,
+    "92x42": true,
+    "92x41": true,
+    "92x40": true,
+
+    /* BOTTOM ROTUNDA TABLES */
+    "83x57": true,
+    "84x57": true,
+    "83x56": true,
+    "84x56": true,
+    "83x53": true,
+    "84x53": true,
+    "83x52": true,
+    "84x52": true,
+    "87x57": true,
+    "88x57": true,
+    "87x56": true,
+    "88x56": true,
+    "87x53": true,
+    "88x53": true,
+    "87x52": true,
+    "88x52": true,
+
+    /* LIBRARY SHELVES */
+    "93x34": true,
+    "94x34": true,
+    "95x34": true,
+    "96x34": true,
+    "97x34": true,
+    "93x32": true,
+    "94x32": true,
+    "95x32": true,
+    "96x32": true,
+    "97x32": true,
+    "98x32": true,
+    "99x32": true,
+    "100x32": true,
+    "96x36": true,
+    "97x36": true,
+    "98x36": true,
+    "99x36": true,
+    "100x36": true,
+    "101x36": true,
+    "99x34": true,
+    "100x34": true,
+    "101x34": true,
+    "102x34": true,
+    "102x32": true,
+    "103x32": true,
+
+    /* GYM ROTUNDA WALLS AND TABLES */
+    "72x34": true,
+    "73x34": true,
+    "73x35": true,
+    "74x35": true,
+    "75x35": true,
+    "76x35": true,
+    "77x35": true,
+    "77x34": true,
+    "78x33": true,
+    "78x32": true,
+    "79x33": true,
+    "79x32": true,
+    "80x34": true,
+    "81x34": true,
+    "82x33": true,
+    "82x32": true,
+    "83x33": true,
+    "83x32": true,
+    "84x34": true,
+    "85x34": true,
+    "86x34": true,
+    "87x34": true,
+    "78x34": true,
+    "79x34": true,
+    "80x35": true,
+    "81x35": true,
+    "82x35": true,
+    "83x35": true,
+    "84x35": true,
+    "87x37": true,
+    "87x37": true,
+    "87x38": true,
+    "87x39": true,
+    "87x40": true,
+    "78x28": true,
+    "79x28": true,
+    "78x29": true,
+    "79x29": true,
+    "82x28": true,
+    "83x28": true,
+    "82x29": true,
+    "83x29": true,
+
+    /* INNER WALLS AND BORDERS */
+    "75x25": true,
+    "75x26": true,
+    "75x27": true,
+    "74x28": true,
+    "73x29": true,
+    "72x30": true,
+    "71x31": true,
+    "70x32": true,
+    "69x33": true,
+    "68x34": true,
+    "67x34": true,
+    "66x34": true,
+    "65x34": true,
+    "65x33": true,
+    "66x33": true,
+    "67x32": true,
+    "67x31": true,
+    "67x30": true,
+    "67x29": true,
+    "66x28": true,
+    "65x28": true,
+    "64x28": true,
+    "63x28": true,
+    "62x28": true,
+    "61x28": true,
+    "60x28": true,
+    "59x28": true,
+    "58x29": true,
+    "58x30": true,
+    "59x31": true,
+    "60x31": true,
+    "61x31": true,
+    "62x31": true,
+    "62x32": true,
+    "63x33": true,
+    "63x34": true,
+    "62x34": true,
+    "61x34": true,
+    "60x34": true,
+    "59x34": true,
+    "58x34": true,
+    "57x34": true,
+    "56x34": true,
+    "55x34": true,
+    "54x34": true,
+    "53x34": true,
+    "52x34": true,
+    "51x34": true,
+    "50x34": true,
+    "49x34": true,
+    "48x34": true,
+    "47x34": true,
+
+    /* TOP LEFT ROOM INTERIOR */
+    "11x9": true,
+    "11x10": true,
+    "11x11": true,
+    "12x11": true,
+    "14x11": true,
+    "14x10": true,
+    "14x9": true,
+    "13x9": true,
+  }
+};
+
+//Misc Helpers
+function getKeyString(x, y) {
+  return `${x}x${y}`;
+}
+
+function isSolid(x,y) {
+  const blockedNextSpace = mapData.blockedSpaces[getKeyString(x, y)];
+  return (
+    blockedNextSpace ||
+    x >= mapData.maxX ||
+    x < mapData.minX ||
+    y >= mapData.maxY ||
+    y < mapData.minY
+  )
+}
+
+
+(function () {
+
+  let playerId;
+  let playerRef;
+  let players = {};
+  let playerElements = {};
+  const startingX = 133;
+  const startingY = 17;
+
+  const gameContainer = document.querySelector(".game-container");
+
+  function handleArrowPress(xChange=0, yChange=0) {
+    const newX = players[playerId].x + xChange;
+    const newY = players[playerId].y + yChange;
+    if (!isSolid(newX, newY)) {
+      //move to the next space
+      players[playerId].x = newX;
+      players[playerId].y = newY;
+      if (xChange === 1) {
+        players[playerId].direction = "right";
+      }
+      if (xChange === -1) {
+        players[playerId].direction = "left";
+      }
+      playerRef.set(players[playerId]);
+    }
+  }
+
+  function initGame() {
+
+    new KeyPressListener("ArrowUp", () => handleArrowPress(0, -1))
+    new KeyPressListener("ArrowDown", () => handleArrowPress(0, 1))
+    new KeyPressListener("ArrowLeft", () => handleArrowPress(-1, 0))
+    new KeyPressListener("ArrowRight", () => handleArrowPress(1, 0))
+    new KeyPressListener("KeyW", () => handleArrowPress(0, -1))
+    new KeyPressListener("KeyS", () => handleArrowPress(0, 1))
+    new KeyPressListener("KeyA", () => handleArrowPress(-1, 0))
+    new KeyPressListener("KeyD", () => handleArrowPress(1, 0))
+
+    const allPlayersRef = firebase.database().ref(`players`);
+
+    allPlayersRef.on("value", (snapshot) => {
+      //Fires whenever a change occurs
+      players = snapshot.val() || {};
+      Object.keys(players).forEach((key) => {
+        const characterState = players[key];
+        let el = playerElements[key];
+        // Now update the DOM
+        el.querySelector(".Character_name").innerText = characterState.name;
+        el.setAttribute("data-color", characterState.color);
+        el.setAttribute("data-direction", characterState.direction);
+        const left = 16 * (characterState.x - players[playerId].x + 12) + "px";
+        const top = 16 * (characterState.y - players[playerId].y + 7) - 1 + "px";
+        el.style.transform = `translate3d(${left}, ${top}, 0)`;
+
+        if (key == playerId) {
+
+          console.log(players[playerId].x + ", " + players[playerId].y);
+
+          const ML = ((startingX - players[playerId].x) * 16) + 'px';
+          const MT = ((startingY - players[playerId].y) * 16) + 'px';
+
+
+          document.querySelector(".mapUpper").style.transform = `translate3d(${ML}, ${MT}, 0)`;
+          document.querySelector(".mapLower").style.transform = `translate3d(${ML}, ${MT}, 0)`;
+
+          if (players[playerId].x === 133 && players[playerId].y === 13) {
+            players[playerId].x = 85;
+            players[playerId].y = 61;
+          }
+
+        }
+      })
+    })
+    allPlayersRef.on("child_added", (snapshot) => {
+      //Fires whenever a new node is added the tree
+      const addedPlayer = snapshot.val();
+      const characterElement = document.createElement("div");
+      characterElement.classList.add("Character", "grid-cell");
+      if (addedPlayer.id === playerId) {
+        characterElement.classList.add("you");
+      }
+      characterElement.innerHTML = (`
+        <div class="Character_shadow grid-cell"></div>
+        <div class="Character_sprite grid-cell"></div>
+        <div class="Character_name-container">
+          <span class="Character_name"></span>
+        </div>
+        <div class="Character_you-arrow"></div>
+      `);
+      playerElements[addedPlayer.id] = characterElement;
+
+      //Fill in some initial state
+      characterElement.querySelector(".Character_name").innerText = addedPlayer.name;
+      characterElement.setAttribute("data-color", addedPlayer.color);
+      characterElement.setAttribute("data-direction", addedPlayer.direction);
+      const left = 16 * 12 + "px";
+      const top = 16 * 7 - 1 + "px";
+      characterElement.style.transform = `translate3d(${left}, ${top}, 0)`;
+      gameContainer.appendChild(characterElement);
+    })
+
+
+    //Remove character DOM element after they leave
+    allPlayersRef.on("child_removed", (snapshot) => {
+      const removedKey = snapshot.val().id;
+      gameContainer.removeChild(playerElements[removedKey]);
+      delete playerElements[removedKey];
+    })
+
+  }
+
+  firebase.auth().onAuthStateChanged((user) => {
+    console.log(user)
+    if (user) {
+      //You're logged in!
+      playerId = user.uid;
+      playerRef = firebase.database().ref(`players/${playerId}`);
+
+      const name = "Tyson";
+
+      playerRef.set({
+        id: playerId,
+        name,
+        direction: "right",
+        color: "green",
+        x:startingX,
+        y:startingY,
+      })
+
+      //Remove me from Firebase when I diconnect
+      playerRef.onDisconnect().remove();
+
+      //Begin the game now that we are signed in
+      initGame();
+    } else {
+      //You're logged out.
+    }
+  })
+
+  firebase.auth().signInAnonymously().catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    console.log(errorCode, errorMessage);
+  });
+
+
+})();
